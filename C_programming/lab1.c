e2026cf1451618731c34fc432984d99aafe23aac
matkaingo <mateogomezrios17@gmail.com>
2022-01-07 09:26:25 -0500

    Basic API and first lab

--- C_programming/lab1.c
+++ C_programming/lab1.c
@@ -1,9 +1,6 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-
 char *ip;
 char word1[100];
 char word2[100];
@@ -11,15 +8,10 @@ int op = 1;
 int ip_len;
 int try_again = 0;
 int p;
-
 int ip_validation(char *ip);
 int ConvertAscii(int number);
 int randomize();
 char *concat(char word1[100], char word2[100]);
-
-
-
-
 int main(void) {
 	while (op != 0) {
         if(try_again == 1){
@@ -35,11 +27,6 @@ int main(void) {
             puts("0: Salir");
             scanf("%d", &op);
         }
-
-		
-		
-		
-		
 		if (op == 1) {
             system("clear");
             ip = malloc(256);
@@ -68,26 +55,20 @@ int main(void) {
 			puts("Escribe el numero Ascii a convertir");
 			scanf("%d", &number);
             printf("\n Ascii => %c",ConvertAscii(number));
-
             puts("\n \n \n....Escribe 1(Para volver a hacerlo) o Escribe 0(para volver al menu)");
 			scanf("%d", &try_again);
             p=2;
-			
 		}
-		
 		else if (op == 3) {
             system("clear");
             puts("Generar un numero aleatorio entre 1 y 6");
             puts("---------------------------------------");
 			puts("El numero random generado es:");
 			printf("El numero random generado es => %d", randomize());
-
-
             puts("\n \n \n....Escribe 1(Para volver a hacerlo) o Escribe 0(para volver al menu)");
 			scanf("%d", &try_again);
             p=3;
 		}
-		
 		else if (op == 4) {
             system("clear");
             puts("Concatenar (juntar) dos palabras ingresadas por consola");
@@ -98,31 +79,22 @@ int main(void) {
 			puts("Escribe Segunda palabra");
             scanf("%s", word2);
             printf("Las palabras quedarian asi => %s", concat(word1,word2));
-			
-
-
-
             puts("\n \n \n....Escribe 1(Para volver a hacerlo) o Escribe 0(para volver al menu)");
 			scanf("%d", &try_again);
             p=4;
 		}
-		
 		else if (op == 0) {
-			printf("Apagando...\n");
+			system("clear");
 		}
-		
 		else {
 			printf("Opcion no valida\n");
 		}
 	}
 }
-
-//Retorna 1 si es valido y 0 si no lo es
-int ip_validation(char *ip){
+int ip_validation(char *ip){ //Retorna 1 si es valido y 0 si no lo es
     char *ip_split;
     int bool =1;
     int count = 0;
-    int ip_size;
     ip_split = strtok(ip, ".");
     while (ip_split != NULL && bool ==1)
     {
@@ -131,17 +103,12 @@ int ip_validation(char *ip){
         }
         ip_split = strtok(NULL, ".");
         count ++;
-
     }
     if(count!=4){
         bool = 0;
     }
-    
     return bool;
 }
-
 int ConvertAscii(int number){char ascii = number; return ascii; }
-
 int randomize(){return 1 + rand() / (RAND_MAX / (6 - 1 + 1) + 1);}
-
 char *concat(char word1[100], char word2[100]){return strcat(word1, word2);}
